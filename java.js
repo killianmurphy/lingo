ea3d75202a9c76d046785f2725ce9c4c239acdd6
killian <99059444@mydavinci.nl>
2020-01-07 12:57:18 +0100

    begin gemaakt

---
+++ java.js
@@ -0,0 +1,9 @@
+var input = "appel"
+var playing - true;
+var pogingen = 0;
+var word = words[math.floor(math.random() * words.length)];
+
+console.log(word)
+for (var c = 0; c < word.length; c++) {
+	console.log(word.charAT(c));
+
