d7b4e7f3076bb493c52ad8d39d02869af12eb0dc
killian <99059444@mydavinci.nl>
2020-01-15 09:26:50 +0100

    oefening van workshop toegevoegd

--- java.js
+++ java.js
@@ -1,9 +1,71 @@
-var input = "appel"
-var playing - true;
-var pogingen = 0;
-var word = words[math.floor(math.random() * words.length)];
 
-console.log(word)
-for (var c = 0; c < word.length; c++) {
-	console.log(word.charAT(c));
+var checkWoord = document.getElementById("checkWoord");
+var button = document.getElementById("buttonCheck");
+button.setAttribute("onclick", "checkWord(word)");
+
+function checkWord(){
+	raadWord = "tafel";
+ raadWord = raadWord.value.split("");
+ checkWoord = checkWoord.split("");
+ console.log(raadWord);
+ console.log(checkWoord);
+for (var i=0; i<raadWord.length; i++){
+      if(checkWoord[i] == raadWord[i]){
+ 	    document.getElementById("letter_" + (i+1)).
+ 	    innerHTML = raadWord[i];
+ 	    document.getElementById("letter_" + (i+1)).style.
+ 	    backgroundColor = "green";
+ 	    raadWord[i] = "*";
+
+    }
+  }
+   console.log(raadWord);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var word = document.getElementById("word");
+
+// var letter = document.getElementById("letter");
+
+// var button = document.getElementById("buttonCheck");
+// button.setAttribute("onclick", "checkWord(word)");
+
+// function checkWord(event){
+//  var letters = event.value.split("");
+//  console.log(letters);
+// for (var i=0; i<letters.length; i++){
+//       if(letter.value == letters[i]){
+//  	    document.getElementById("letter_" + (i+1)).innerHTML =
+//  	    letters[i];
+
+//     }
+//   }
+// }
 
