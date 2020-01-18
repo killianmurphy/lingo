543f8fcf319247a0734afabd5f3747c404393eb7
killian <99059444@mydavinci.nl>
2020-01-18 18:55:49 +0100

    uitleg toegevoegd

--- java.js
+++ java.js
@@ -1,71 +1,53 @@
 
-var checkWoord = document.getElementById("checkWoord");
-var button = document.getElementById("buttonCheck");
-button.setAttribute("onclick", "checkWord(word)");
+var words = words [Math.floor(Math.random) * words.length];
+var beurt = 0;
 
-function checkWord(){
-	raadWord = "tafel";
- raadWord = raadWord.value.split("");
- checkWoord = checkWoord.split("");
- console.log(raadWord);
- console.log(checkWoord);
-for (var i=0; i<raadWord.length; i++){
-      if(checkWoord[i] == raadWord[i]){
- 	    document.getElementById("letter_" + (i+1)).
- 	    innerHTML = raadWord[i];
- 	    document.getElementById("letter_" + (i+1)).style.
- 	    backgroundColor = "green";
- 	    raadWord[i] = "*";
+var spelbord = document.createElement("div");
+spelbord.setAttribute('class', 'spelbord')
+document.body.appendChild(spelbord);
 
-    }
-  }
-   console.log(raadWord);
+for(i=0;i<5;i++){
+	var row = document.createElement(div);
+
+	for(j=0;j<5;j++){
+		var vakje = document.createElement("h1");
+		vakje.setAttribute('id', `rij${i}letter${j}`);
+		vakje.setAttribute('class', 'letterbox');
+		spelbord.appendChild();
+	}
 }
 
+var wordArray = word.split('');
+var knop = document.createElement('button');
+knop.setAttribute('onclick', 'check()');
+var inputBox = document.createElement('input');
+knop.innerText = 'controleer'
+inputBox.setAttribute('type', 'text');
+spelbord.appendChild(inputBox);
+spelbord.appendChild(knop);
 
+document.getElementById('rij0letter0'.innerText = wordArray[0]);
 
+function check(){ 
+	var guess = inputBox.value;
+	var guessArray = guess.split('');
 
+	guessArray.forEach(function(value, index){
+		if(value == wordArray[index]){
+			document.getElementById(rij${beurt}letter${index}.style.backgroundColor = 'green';
+			document.getElementById(rij${beurt}letter${index}.innerText = value;
+			guessArray[index] = wordArray[index] = null;
+		}
+	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var word = document.getElementById("word");
-
-// var letter = document.getElementById("letter");
-
-// var button = document.getElementById("buttonCheck");
-// button.setAttribute("onclick", "checkWord(word)");
-
-// function checkWord(event){
-//  var letters = event.value.split("");
-//  console.log(letters);
-// for (var i=0; i<letters.length; i++){
-//       if(letter.value == letters[i]){
-//  	    document.getElementById("letter_" + (i+1)).innerHTML =
-//  	    letters[i];
-
-//     }
-//   }
-// }
-
+	guessArray.forEach(function(value, index){
+		if(value != null){
+			if(wordArray.indexOf(value) > -1){
+				document.getElementById(rij${beurt}letter${index}.style.backgroundColor = "yellow"
+				document.getElementById(rij${beurt}letter${index}.innerText = 
+				wordArray[wordArray.indexOf(value)] = null;
+				guess[index] = null;
+			}
+		}
+	});
+}
